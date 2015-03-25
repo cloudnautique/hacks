454b17a6f014487f8a2ca1e5159c2e1612866730
Bill Maxwell <bill@rancher.com>
2015-03-24 21:51:01 -0700

    yep... :-\

--- ros-crowbar.sh
+++ ros-crowbar.sh
@@ -26,8 +26,8 @@ rancher:
 EOF
 
 cd /boot
-curl -L -O vmlinux-rancheros-0.2.1 https://github.com/rancherio/os/releases/download/v0.2.1/vmlinuz
-curl -L -O initrd-rancheros-0.2.1 https://github.com/rancherio/os/releases/download/v0.2.1/initrd
+curl -L -o vmlinux-rancheros-0.2.1 https://github.com/rancherio/os/releases/download/v0.2.1/vmlinuz
+curl -L -o initrd-rancheros-0.2.1 https://github.com/rancherio/os/releases/download/v0.2.1/initrd
 
 cat<<EOF
 menuentry 'RancherOS-v0.2.1' {
@@ -38,7 +38,7 @@ menuentry 'RancherOS-v0.2.1' {
         set root='(/dev/mapper/vda,msdos1)'
         search --no-floppy --fs-uuid --set=root ea795a0f-394e-4ced-952b-0fd0e3ab762c
         echo    'Loading RancherOS ...'
-        linux   /boot/vmlinuz-rancheros-0.2.1 rancher.passwd=rancher rancher.debug=true ro console=ttyS0,38400n8 console=ttyS0
+        linux   /boot/vmlinuz-rancheros-0.2.1 rancher.password=rancher rancher.debug=true ro console=ttyS0,38400n8 console=ttyS0
         echo    'Loading initial ramdisk ...'
         initrd  /boot/initrd-rancheros-0.2.1
 }
